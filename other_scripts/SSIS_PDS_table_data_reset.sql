ce583a0aa447eeb2f1a3b1c7ac6a7a648ba53e9d
Jon Jaussi <jon@jaussiconsulting.net>
2019-11-17 19:33:13 -0700

    Date and Time dimension

---
+++ other_scripts/SSIS_PDS_table_data_reset.sql
@@ -0,0 +1,9 @@
+TRUNCATE TABLE [SSIS_PDS].[dbo].[PackageErrorLog];
+TRUNCATE TABLE [SSIS_PDS].[dbo].[PackageTaskLog];
+TRUNCATE TABLE [SSIS_PDS].[dbo].[PackageVariableLog];
+DELETE FROM [SSIS_PDS].[dbo].[PackageLog];
+DELETE FROM [SSIS_PDS].[dbo].[BatchLog];
+DELETE FROM [SSIS_PDS].[dbo].[PackageVersion];
+DELETE FROM [SSIS_PDS].[dbo].[Package];
+
+
